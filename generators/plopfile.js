7d9fa2e24ef8d21b35aa33d274d7728407eb9664
Willian Del Freu <williankeyex@gmail.com>
2022-05-17 20:19:44 -0300

    Add Plop

---
+++ generators/plopfile.js
@@ -0,0 +1,19 @@
+export default function (plop) {
+  plop.setGenerator('component', {
+    description: 'application component logic',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'component name please'
+      }
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/index.tsx',
+        templateFile: 'templates/index.tsx.hbs'
+      }
+    ]
+  })
+}
